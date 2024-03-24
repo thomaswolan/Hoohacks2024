a640d85af4e2f424d13041dc215742d8a841d7d0
twolan34 <twolan34@vt.edu>
2024-03-24 11:39:22 -0400

    THing

--- src/about_us.js
+++ src/about_us.js
@@ -8,8 +8,7 @@ function AboutUs() {
       <div className="aboutus-container">
         <h1>About Us</h1>
         <p>Our goal was to demystify the stock market for beginners. We recognized the potential in leveraging technology to simplify the investment process, making it accessible and engaging for everyone. Apollo.ai stands as a testament to our commitment, offering a user-friendly interface that opens the doors to the world of stocks like never before.</p>
-        <h2>Our Team</h2>
-        <p>Our team is composed of experienced developers, designers, and project managers who work collaboratively to deliver successful projects.</p>
+        <p>Apollo.ai was brought to life through a plethora of technologies and frameworks. At its core, the project harnesses the power of React and JavaScript for the frontend, paired seamlessly with Python and Flask powering the backend. Our decision to employ RandomForest for AI analysis, complemented by the robust styling of HTML/CSS and Bootstrap, and containerized with Docker, reflects our dedication to creating a resilient and scalable application.</p>
         <h2>Contact Us</h2>
         <p>If you have any questions or would like to discuss a potential project, please contact us at fake_email@ourcompany.com.</p>
       </div>
