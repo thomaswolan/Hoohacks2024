1fc78fa513aade451aaa9a6ab00e458f13051223
twolan34 <twolan34@vt.edu>
2024-03-23 20:07:58 -0400

    Removed some navbar things

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,7 @@
 
-import React, { useState, useEffect, useRef } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCog } from '@fortawesome/free-solid-svg-icons';
+import React, { useState, useEffect, } from 'react';
 import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from '../assets/img/logo-again.png';
-import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/nav-icon2.svg';
-import navIcon3 from '../assets/img/nav-icon3.svg';
-import { HashLink } from 'react-router-hash-link';
 import {
   BrowserRouter as Router
 } from "react-router-dom";
@@ -47,20 +41,8 @@ export const NavBar = () => {
           </Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Long Term Investments</Nav.Link>
-              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Short Term Investments</Nav.Link>
               <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>About Us</Nav.Link>
             </Nav>
-            <span className="navbar-text">
-              <div className="social-icon">
-                <a href="https://www.linkedin.com/in/thomas-wolan/"><img src={navIcon1} alt="" /></a>
-                <a href="https://www.instagram.com/twolan34/"><img src={navIcon3} alt="" /></a>
-                <a href="https://www.facebook.com/tom.wolan.5/"><img src={navIcon2} alt="" /></a>
-              </div>
-              <HashLink to='#connect'>
-                <button className="vvd"><span>Let’s Connect</span></button>
-              </HashLink>
-            </span>
           </Navbar.Collapse>
         </Container>
       </Navbar>
