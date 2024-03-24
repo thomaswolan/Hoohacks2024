b55324b2e145d8ba8e3bf40a7e577e713ee4e318
twolan34 <twolan34@vt.edu>
2024-03-23 21:01:58 -0400

    Added fade effect when going to About Us

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
-import { Skills } from "./components/Skills";
-import { Projects } from "./components/Projects";
-import { Contact } from "./components/Contact";
-import { Footer } from "./components/Footer";
 import AboutUs from './about_us'; // Assuming you have an AboutUs component
+import { Footer } from "./components/Footer";
+import React from 'react';
+
+
+const AnimatedRoutes = () => {
+  const location = useLocation();
+
+
+  return (
+    <TransitionGroup component={null}>
+      <CSSTransition key={location.pathname} classNames="fade" timeout={300}>
+        <Routes location={location}>
+          <Route path="/about_us" element={<AboutUs />} />
+          <Route path="/" element={<Banner />} />
+        </Routes>
+      </CSSTransition>
+    </TransitionGroup>
+  );
+};
+
 
 function App() {
   return (
     <Router>
       <div className="App">
         <NavBar />
-        <Routes>
-          <Route path="/about_us" element={<AboutUs />} />
-          <Route path="/" element={<><Banner /><Skills /><Projects /><Contact /></>} />
-        </Routes>
+        <AnimatedRoutes /> {/* Use AnimatedRoutes component here */}
         <Footer />
       </div>
     </Router>
   );
 }
 
+
 export default App;
+
+
